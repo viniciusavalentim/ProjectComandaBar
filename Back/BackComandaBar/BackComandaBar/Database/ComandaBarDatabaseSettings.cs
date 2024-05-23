6e520907522c1723c579d752d4c6d2193b3d5636
Vinicius Valentim <viniciusvalentim769@gmail.com>
2024-05-23 12:59:31 -0300

    Aplicando o CRUD completo

--- Back/BackComandaBar/BackComandaBar/Database/ComandaBarDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BackComandaBar.Database
-{
-    public class ComandaBarDatabaseSettings
-    {
-        public string ConnectionString { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-        public string ComandaBarCollectionName { get; set; } = null!;
-    }
-}
